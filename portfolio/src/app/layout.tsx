0a5dae1d8f97511fee6f5a50d121f672ecb2c3fb
rophpad <rophenp@gmail.com>
2025-09-24 08:33:20 +0100

    Add suppressHydrationWarning to RootLayout HTML element

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body
         className={` ${roboto.variable} antialiased text-[15px]  min-h-screen `}
       >
